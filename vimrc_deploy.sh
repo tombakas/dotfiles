abaec54edce5a757a215cc456f05d3ee7e781ca4
laserpas <laserpas@robincop.laserpas.com>
2015-07-27 01:18:10 +0000

    Fixed error in vim_deploy.sh, added additional checks.

--- vimrc_deploy.sh
+++ vimrc_deploy.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 mkdir -p ~/.vim/bundle/
 mkdir -p ~/.vim/colors/
+
 if [ -f ./Mustang.vim ]
 then
-    ln -s `pwd`/Mustang.vim /home/tomas/.vim/colors/Mustang.vim
+    if [ ! -L ~/.vim/colors/Mustang.vim ]
+    then
+        echo "Creating symlink for Mustang.vim in ~/.vim/colors/"
+        ln -s $(pwd)/Mustang.vim /home/$(whoami)/.vim/colors/Mustang.vim
+    else
+        echo "Symlink for Mustang already exists."
+    fi
 fi
 
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+if [ ! -d ~/.vim/bundle/neobundle.vim ]
+then
+    echo "Cloning neobundle.vim into ~/.vim/bundles/"
+    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+else
+    echo "Neobundle.vim already exists in ~/.vim/bundles/."
+fi
