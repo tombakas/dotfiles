8583e5ec6fbe6985c7aec01d877f3a59764a9775
Tomas Tombakas <tombakas@gmail.com>
2016-11-14 14:41:58 +0200

    Update subdircount script

--- utils/subdircount.sh
+++ utils/subdircount.sh
@@ -2,5 +2,5 @@
 
 find . -maxdepth 1 -type d ! -name \.\* | while read dir; do
     printf "%-25.25s : " "$dir" 
-    find "$dir" -type f | wc -l
-done
+    find "$dir" -maxdepth 1 ! -name ${dir:2} | wc -l
+done | sort -k 3 -n -r
