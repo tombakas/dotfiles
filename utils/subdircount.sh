6ae28971d1b4e02da9efc2aeacd39b17eb3f052b
Tomas Tombakas <tombakas@gmail.com>
2016-11-15 11:36:17 +0200

    Account for names with spaces

--- utils/subdircount.sh
+++ utils/subdircount.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find . -maxdepth 1 -type d ! -name \.\* | while read dir; do
+find . -maxdepth 1 -type d ! -name \.\* -print0 | while IFS= read -r -d '' dir; do
     printf "%-25.25s : " "$dir" 
-    find "$dir" -maxdepth 1 ! -name ${dir:2} | wc -l
+    find "$dir" -maxdepth 1 ! -name "${dir:2}" | wc -l
 done | sort -k 3 -n -r
