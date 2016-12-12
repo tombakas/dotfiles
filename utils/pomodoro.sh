2db475755e76181b6949fea366d9ea4e0f50bb93
Tomas Tombakas <tombakas@gmail.com>
2016-12-12 18:04:56 +0200

    Add primitive pomodoro timer

---
+++ utils/pomodoro.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+interval=1
+pom_stop=0
+work_interval=1500
+break_interval=300
+
+trap ctrl_c INT
+
+ctrl_c() {
+    printf "\n Pomodoro timer paused\n\n"
+    echo "[c]ontinue or [q]uit?"
+
+    read answer
+
+    if [ "${answer:0:1}" == "c" ]; then
+        pom_stop=0
+    elif [ "${answer:0:1}" == "q" ]; then
+        pom_stop=1
+    fi
+}
+
+while [ $pom_stop -ne 1 ]; do
+
+    for s in $(eval echo {1..$work_interval}); do
+        if [ $pom_stop -eq 1 ]; then
+            break
+        fi
+
+        if [ $(($s%$interval)) -eq 0 ] || [ $s -eq 1 ]; then
+            min=$((($work_interval-$s) / 60))
+            sec=$((($work_interval-$s) % 60))
+            printf "\033cPomodoro timer\n\n" # Clear screen
+            printf "Time till break: %02d:%02d\n" $min $sec
+        fi
+        sleep 1
+    done
+    
+    if [ $pom_stop -eq 0 ]; then notify-send "Break!"; fi
+
+    for s in $(eval echo {1..$break_interval}); do
+        if [ $pom_stop -eq 1 ]; then
+            break
+        fi
+
+        if [ $(($s%$interval)) -eq 0 ] || [ $s -eq 1 ]; then
+            min=$((($break_interval-$s) / 60))
+            sec=$((($break_interval-$s) % 60))
+            printf "\033cBreak timer\n\n" # Clear screen
+            printf "Time till break: %02d:%02d\n" $min $sec
+        fi
+        sleep 1
+    done
+
+done
