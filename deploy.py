61c9700f6d4176a2a1c92478e19656ebd8be5082
Tomas Tombakas <tombakas@gmail.com>
2017-04-29 21:36:59 +0300

    Additional dotfiles, refined logic

--- deploy.py
+++ deploy.py
@@ -19,13 +19,16 @@ sexy_print = sexy_print()
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Dotfile deployment script.')
     parser.add_argument('-v', '--vim', default=False, action='store_true', help='Set up vim.')
+    parser.add_argument('-b', '--bash', default=False, action='store_true', help='Set up bash dotfiles.')
     parser.add_argument('-n', '--nvim', default=False, action='store_true', help='Set up neovim.')
+    parser.add_argument('-m', '--mustang', default=False, action='store_true', help='Set up Mustang colorscheme.')
+    parser.add_argument('--vimplug', default=False, action='store_true', help='Set up vimplug.')
     parser.add_argument(
         '-d',
         '--dotfiles',
         nargs="*",
         default=None,
-        help='Set up dotfiles([b]ash, [v]im, [n]eovim, [t]mux). Defaults to all.')
+        help='Set up dotfiles([b]ash, [v]im, [n]eovim, [t]mux, [m]ustang). Defaults to all.')
     parser.add_argument('-y', default=False, action='store_true', help='Yes to all prompts.')
     args = parser.parse_args()
 
@@ -53,18 +56,13 @@ def create_symlink(source, target):
     elif os.path.islink(full_target):
         print("Symlink ", end="")
         sexy_print.red("Symlink ", target, " is broken, unlinking... ")
+    else:
+        directory_make(os.path.dirname(full_target))
 
     print("Creating symlink to ", end="")
     sexy_print.green(full_target)
-    try:
-        os.symlink(os.path.join(BASE_DIR, source), full_target)
-    except OSError:
-        print("Full path to {} does not exist".format(full_target))
-        sexy_print.red("Exiting")
-        sys.exit(1)
+    os.symlink(os.path.join(BASE_DIR, source), full_target)
     print()
-
-    return
 
 
 def setup_dotfile(source, target, name=None):
@@ -73,6 +71,42 @@ def setup_dotfile(source, target, name=None):
     else:
         sexy_print.header("Setting up " + target)
     create_symlink(source, target)
+
+
+def text_in_file(text, file):
+    text_present = False
+    with open(os.path.expanduser(file), 'r') as f:
+        for line in f:
+            if text in line:
+                text_present = True
+                break
+    return text_present
+
+
+def append_to_file(text, file):
+    print("Adding ", end="")
+    sexy_print.green(text, end="")
+    print(" to {}".format(file))
+    with open(os.path.expanduser(file), "a") as f:
+        f.write(text + "\n")
+
+
+def add_line_to_file(line, file):
+    if not text_in_file(line, file):
+        append_to_file(line, file)
+    else:
+        sexy_print.yellow("\"{}\" ".format(line), end="")
+        print("already in ", end="")
+        sexy_print.yellow("{}".format(file))
+
+
+def setup_bash():
+    setup_dotfile(os.path.join(BASE_DIR, ".git-prompt.sh"), "~/.git-prompt.sh")
+    setup_dotfile(os.path.join(BASE_DIR, ".git_aliases"), "~/.git_aliases")
+    setup_dotfile(os.path.join(BASE_DIR, ".tomasrc"), "~/.tomasrc")
+    add_line_to_file(". ~/.tomasrc", "~/.bashrc")
+    add_line_to_file(". ~/.git-prompt.sh", "~/.bashrc")
+    add_line_to_file(". ~/.git_aliases", "~/.bashrc")
 
 
 def directory_make(target):
@@ -87,6 +121,9 @@ if __name__ == "__main__":
 
     VIM = arguments.vim
     NVIM = arguments.nvim
+    BASH = arguments.bash
+    VIMPLUG = arguments.vimplug
+    MUSTANG = arguments.mustang
     DOTFILES = arguments.dotfiles
 
     if DOTFILES is not None:
@@ -98,3 +135,10 @@ if __name__ == "__main__":
             setup_dotfile(os.path.join(BASE_DIR, "vimrc_files/htmldjango.vim"), "~/.vim/indent/htmldjango.vim")
         if "n" in DOTFILES:
             setup_dotfile(os.path.join(BASE_DIR, "vimrc_files/init.vim"), "~/.config/nvim/init.vim")
+        if "m" in DOTFILES:
+            setup_dotfile(os.path.join(BASE_DIR, "vimrc_files/Mustang.vim"), "~/.vim/colors/Mustang.vim")
+        if "b" in DOTFILES:
+            setup_bash()
+
+    if VIMPLUG:
+        set_up_vimplug()
