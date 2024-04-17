10808a0d4894719984311356affeed4e3d096a5e
Tomas Tombakas <tombakas@gmail.com>
2024-04-17 13:41:19 +0200

    Update get_repo_root.sh

--- utils/get_repo_root.sh
+++ utils/get_repo_root.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-REPO_ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
+# Escape .git subdirectory if inside it
+while [[ "$(pwd)" =~ .git ]]; do
+  cd ..
+done
 
+REPO_ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
 if [ -n "$REPO_ROOT" ]; then
   echo "$REPO_ROOT"
 else
