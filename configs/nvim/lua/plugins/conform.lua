d83b5153258055c0f0299012f5a835c111b915ae
Tomas Tombakas <tombakas@gmail.com>
2024-05-30 11:15:43 +0200

    Add sqlfluff for sql formatting

--- configs/nvim/lua/plugins/conform.lua
+++ configs/nvim/lua/plugins/conform.lua
@@ -12,11 +12,6 @@ return {
 
   config = function()
     require("conform").setup({
-      format_on_save = {
-        enabled = false,
-        lsp_fallback = true,
-        timeout_ms = 500,
-      },
 
       formatters_by_ft = {
         html = { "djlint" },
@@ -24,11 +19,51 @@ return {
         lua = { "stylua" },
         python = { "isort", "black" },
         javascript = { "prettier" },
+        sql = { "sqlfluff" },
+        markdown = { "injected" },
       },
 
       formatters = {
+        injected = {
+
+          options = {
+            -- Set to true to ignore errors
+            ignore_errors = false,
+            -- Map of treesitter language to file extension
+            -- A temporary file name with this extension will be generated during formatting
+            -- because some formatters care about the filename.
+            lang_to_ext = {
+              bash = "sh",
+              c_sharp = "cs",
+              elixir = "exs",
+              javascript = "js",
+              julia = "jl",
+              latex = "tex",
+              markdown = "md",
+              python = "py",
+              ruby = "rb",
+              rust = "rs",
+              teal = "tl",
+              typescript = "ts",
+              sql = "sql",
+            },
+            -- Map of treesitter language to formatters to use
+            -- (defaults to the value from formatters_by_ft)
+            lang_to_formatters = {},
+          },
+        },
+
         djlint = {
           prepend_args = { "--indent", "2" },
+        },
+
+        sqlfluff = {
+          inherit = false,
+
+          command = "sqlfluff",
+          args = { "fix", "--dialect=postgres", "--exclude-rules=AM04", "-" },
+          stdin = true,
+          require_cwd = false,
         },
       },
     })
--- configs/nvim/lua/plugins/mason.lua
+++ configs/nvim/lua/plugins/mason.lua
@@ -19,6 +19,7 @@ return {
         "isort",
         "prettier",
         "shellcheck",
+        "sqlfluff",
         "stylua",
       }
     }
