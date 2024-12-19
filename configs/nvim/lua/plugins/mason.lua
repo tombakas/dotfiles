e178a551b25fde4c1ea2db4e07e12967ceb74bec
Tomas Tombakas <tombakas@gmail.com>
2024-12-19 01:44:43 +0100

    Replace navic with dropbar

--- configs/nvim/lua/plugins/mason.lua
+++ configs/nvim/lua/plugins/mason.lua
@@ -3,14 +3,14 @@ return {
 
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim"
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
 
   config = function()
     require("mason").setup()
 
     ----------- generic tools
-    require('mason-tool-installer').setup {
+    require("mason-tool-installer").setup({
       ensure_installed = {
         "black",
         "djlint",
@@ -20,28 +20,13 @@ return {
         "shellcheck",
         "sqlfluff",
         "stylua",
-      }
-    }
+      },
+    })
 
     ----------- lspconfig
     require("mason-lspconfig").setup({
-      ensure_installed = { "bashls", "lua_ls", "pyright", "ts_ls", "yamlls" }
+      ensure_installed = { "bashls", "lua_ls", "pyright", "ts_ls", "yamlls" },
     })
-    require("mason-lspconfig").setup_handlers(vim.tbl_extend(
-      "keep",
-
-      -- The default handler taht will be called for each installed server
-      -- that doesn"t have a dedicated handler.
-      {
-        function(server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup {
-            on_attach = require("utils").on_attach
-          }
-        end
-      },
-
-      -- Dedicated handlers for specific servers.
-      require("lsp_configs"))
-    )
-  end
+    require("mason-lspconfig").setup_handlers(require("lsp_configs"))
+  end,
 }
