11397c25ec826d2e617a5798f07d860ecac7183d
Tomas Tombakas <tombakas@gmail.com>
2025-01-08 23:19:20 +0100

    Replace cmp with blink

---
+++ configs/nvim/lua/plugins/blink.cmp.lua
@@ -0,0 +1,52 @@
+return {
+
+  {
+    "saghen/blink.cmp",
+    dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
+
+    -- use a release tag to download pre-built binaries
+    version = "*",
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = {
+        preset = "default",
+        ["<c-j>"] = { "snippet_forward", "fallback" },
+        ["<c-k>"] = { "snippet_backward", "fallback" },
+      },
+
+      snippets = { preset = "luasnip" },
+
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+        },
+      },
+      signature = { enabled = true },
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- Will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+
+        providers = {
+          path = {
+            opts = {
+              show_hidden_files_by_default = true,
+            },
+          },
+        },
+      },
+    },
+    opts_extend = { "sources.default" },
+  },
+}
--- configs/nvim/lua/plugins/lspconfig.lua
+++ configs/nvim/lua/plugins/lspconfig.lua
@@ -1,10 +1,25 @@
 return {
   'neovim/nvim-lspconfig',
 
-  config = function()
+  opts = {
+    servers = {
+      lua_ls = {},
+      pyrights = {}
+    }
+  },
+
+  config = function(_, opts)
     local keymap = vim.keymap.set
+    local lspconfig = require('lspconfig')
 
     keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
     keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+
+    for server, config in pairs(opts.servers) do
+      -- passing config.capabilities to blink.cmp merges with the capabilities in your
+      -- `opts[server].capabilities, if you've defined it
+      config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+      lspconfig[server].setup(config)
+    end
   end
 }
