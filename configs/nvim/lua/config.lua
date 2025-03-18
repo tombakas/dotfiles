19294aeeaccd6e070f2d35e790869b6f4175267a
Tomas Tombakas <tombakas@gmail.com>
2025-03-18 17:37:13 +0100

    store macos specific settings

--- configs/nvim/lua/config.lua
+++ configs/nvim/lua/config.lua
@@ -46,5 +46,21 @@ opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undodir"
 opt.undofile = true
 opt.undolevels = 10000
 
-vim.o.background="dark"
-vim.cmd.colorscheme("onedark")
+vim.cmd.colorscheme("tokyonight-moon")
+vim.o.background = "dark"
+
+vim.fn.setenv("PATH", "/Users/ttombakas/.local/share/nvim/mason/bin" .. ":" .. vim.fn.getenv("PATH"))
+
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+  vim.g.neovide_input_ime = false
+  -- vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_cursor_vfx_particle_density = 7.0
+  vim.o.shell = "/opt/homebrew/bin/fish"
+  vim.o.guifont = "FiraCode Nerd Font Mono"
+
+  -- vim.cmd("source /Users/ttombakas/Projects/neovim/envrc_loader.lua")
+  -- vim.cmd("cd /Users/ttombakas/Projects/fareharbor.com/")
+  vim.fn.setenv("PATH", "/Users/ttombakas/.local/share/nvim/mason/bin" .. ":" .. vim.fn.getenv("PATH"))
+end
