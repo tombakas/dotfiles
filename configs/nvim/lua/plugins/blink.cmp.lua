8752c32e0978cd573397dfed01b7c35cd3a33a82
Tomas Tombakas <tomas.tombakas@fareharbor.com>
2025-10-13 23:54:35 +0200

    chore: remove avante

--- configs/nvim/lua/plugins/blink.cmp.lua
+++ configs/nvim/lua/plugins/blink.cmp.lua
@@ -53,9 +53,6 @@ return {
           "path",
           "snippets",
           "buffer",
-          "avante_commands",
-          "avante_mentions",
-          "avante_files",
         },
 
         providers = {
@@ -63,24 +60,6 @@ return {
             opts = {
               show_hidden_files_by_default = true,
             },
-          },
-          avante_commands = {
-            name = "avante_commands",
-            module = "blink.compat.source",
-            score_offset = 90, -- show at a higher priority than lsp
-            opts = {},
-          },
-          avante_files = {
-            name = "avante_files",
-            module = "blink.compat.source",
-            score_offset = 100, -- show at a higher priority than lsp
-            opts = {},
-          },
-          avante_mentions = {
-            name = "avante_mentions",
-            module = "blink.compat.source",
-            score_offset = 1000, -- show at a higher priority than lsp
-            opts = {},
           },
         },
       },
